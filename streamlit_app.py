27b9e30a618b0b3b55aa77ed8040b428914bb3a8
anjnosreme <123920719+anjnosreme@users.noreply.github.com>
2023-01-31 18:11:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocad Toast') 
+streamlit.text('🥑🍞Avocado Toast') 
 
 
 
